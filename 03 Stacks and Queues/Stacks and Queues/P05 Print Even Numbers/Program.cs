83d9064d5221ba7218dfeb7dba04d83ac9eac82e
Irina Georgieva <georgieva2490@gmail.com>
2021-09-08 16:22:12 +0200

    Added solutions for the lab Stacks and Queues

---
+++ 03 Stacks and Queues/Stacks and Queues/P05 Print Even Numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            Queue<int> lineNumbers = new Queue<int>(numbers);
+            int count = lineNumbers.Count;
+
+            Queue<int> evenNumbers = new Queue<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                if (lineNumbers.Peek() % 2 == 0)
+                {
+                    evenNumbers.Enqueue(lineNumbers.Dequeue());
+                }
+                else
+                {
+                    lineNumbers.Dequeue();
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", evenNumbers));
+
+        }
+    }
+}
---
+++ 03 Stacks and Queues/Stacks and Queues/P06 Supermarket/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> customers = new Queue<string>();
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                if(command == "Paid")
+                {
+                    while(customers.Count != 0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                customers.Enqueue(command);
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
