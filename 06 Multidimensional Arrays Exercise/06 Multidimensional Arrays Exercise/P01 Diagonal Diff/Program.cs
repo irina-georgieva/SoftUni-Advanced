51aa6b43254b8842ab8727582b03daf22649da41
Irina Georgieva <georgieva2490@gmail.com>
2021-09-15 18:43:19 +0200

    Added new theme

---
+++ 06 Multidimensional Arrays Exercise/06 Multidimensional Arrays Exercise/P01 Diagonal Diff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_Diagonal_Diff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
