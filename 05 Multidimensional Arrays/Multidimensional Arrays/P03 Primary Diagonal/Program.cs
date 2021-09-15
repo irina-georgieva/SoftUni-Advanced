a32ce3eca59700a497b045416c631390522f0172
Irina Georgieva <georgieva2490@gmail.com>
2021-09-15 18:23:46 +0200

    Added new exercises

---
+++ 05 Multidimensional Arrays/Multidimensional Arrays/P03 Primary Diagonal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            
+            for (int row = 0; row < n; row++)
+            {
+                int[] arr = ReadArrayFromConsole();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i,i];
+            }
+
+            Console.WriteLine(sum);
+        }
+
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ 05 Multidimensional Arrays/Multidimensional Arrays/P04 Symbol in Matrix/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace P04_Symbol_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string arr = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            char symbol = char.Parse(Console.ReadLine());
+            bool isFound = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                if (isFound)
+                {
+                    break;
+                }
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{symbol} does not occur in the matrix");
+            }
+        }
+    }
+}
