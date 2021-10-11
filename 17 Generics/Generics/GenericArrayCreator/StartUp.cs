dda65557b07b10ea965aaab4a70db504e3f04476
Irina Georgieva <georgieva2490@gmail.com>
2021-10-11 17:01:43 +0200

    Added new solutions

---
+++ 17 Generics/Generics/GenericArrayCreator/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] name = ArrayCreator.Create(5, "Pesho");
+        }
+    }
+}
