7cd4dc8a7f627442833fc6d4d79635d436bed85c
Irina Georgieva <georgieva2490@gmail.com>
2021-10-18 22:37:49 +0200

    Exam Preparation

---
+++ Mid Exam Preparation/01 Advanced Retake Exam - 18 Aug 2021/RetakeExam-18Aug2021/BirthdayCelebration/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] guestNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> guestValue = new Queue<int>(guestNumber);
+
+            int[] plateNumber = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> plates = new Stack<int>(plateNumber);
+
+            int wastedFood = 0;
+
+            while (guestValue.Count > 0 && plates.Count > 0)
+            {
+                int currentGuest = guestValue.Peek();
+                int currentPlate = plates.Peek();
+
+                if (currentGuest < currentPlate)
+                {
+                    wastedFood += currentPlate - currentGuest;
+                    guestValue.Dequeue();
+                    plates.Pop();
+                }
+                else if (currentGuest == currentPlate)
+                {
+                    guestValue.Dequeue();
+                    plates.Pop();
+                }
+                else if (currentGuest > currentPlate)
+                {
+                    while (currentGuest > 0 && plates.Count > 0)
+                    {
+                        currentGuest = currentGuest - currentPlate;
+                        plates.Pop();
+                        if (plates.Count > 0)
+                        {
+                            currentPlate = plates.Peek();
+
+                            if (currentGuest < currentPlate)
+                            {
+                                wastedFood += currentPlate - currentGuest;
+                                currentGuest = 0;
+                                guestValue.Dequeue();
+                                plates.Pop();
+                            }
+                            else if (currentGuest == currentPlate)
+                            {
+                                currentGuest = 0;
+                                guestValue.Dequeue();
+                                plates.Pop();
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            if (guestValue.Count == 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", plates)}");
+                Console.WriteLine($"Wasted grams of food: {wastedFood}");
+            }
+            else if (plates.Count == 0)
+            {
+                Console.WriteLine($"Guests: {string.Join(" ", guestValue)}");
+                Console.WriteLine($"Wasted grams of food: {wastedFood}");
+            }
+        }
+    }
+}
---
+++ Mid Exam Preparation/01 Advanced Retake Exam - 18 Aug 2021/RetakeExam-18Aug2021/TheBattleOfTheFiveArmies/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Linq;
+
+namespace TheBattleOfTheFiveArmies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int armor = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+
+            char[][] battleField = new char[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                battleField[row] = Console.ReadLine().Select(x => x).ToArray();
+            }
+
+            int armyRow = 0;
+            int armyCol = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < battleField[row].Length; col++)
+                {
+                    if (battleField[row][col] == 'A')
+                    {
+                        armyRow = row;
+                        armyCol = col;
+                        battleField[armyRow][armyCol] = '-';
+                    }
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (armor > 0)
+            {
+                string[] commandArgs = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string direction = commandArgs[0];
+                int orcRow = int.Parse(commandArgs[1]);
+                int orcCol = int.Parse(commandArgs[2]);
+                battleField[orcRow][orcCol] = 'O';
+
+                if (direction == "up" && IsCellValid(armyRow - 1, armyCol,
+                    rows, battleField[armyRow].Length))
+                {
+                    armyRow = armyRow - 1;
+                }
+
+                else if (direction == "down" && IsCellValid(armyRow + 1, armyCol,
+                    rows, battleField[armyRow].Length))
+                {
+                    armyRow = armyRow + 1;
+
+                }
+                else if (direction == "left" && IsCellValid(armyRow, armyCol - 1,
+                    rows, battleField[armyRow].Length))
+                {
+
+                    armyCol = armyCol - 1;
+                }
+
+                else if (direction == "right" && IsCellValid(armyRow, armyCol + 1,
+                    rows, battleField[armyRow].Length))
+                {
+
+                    armyCol = armyCol + 1;   
+                }
+
+                armor--;
+
+                if (armor <= 0)
+                {
+                    battleField[armyRow][armyCol] = 'X';
+                    Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+                    break;
+                }
+
+                if (battleField[armyRow][armyCol] == 'O')
+                {
+                    armor -= 2;
+                    if (armor > 0)
+                    {
+                        battleField[armyRow][armyCol] = '-';
+                    }
+                    else if (armor <= 0)
+                    {
+                        battleField[armyRow][armyCol] = 'X';
+                        Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+                        break;
+                    }
+                }
+                else if (battleField[armyRow][armyCol] == 'M')
+                {
+                    battleField[armyRow][armyCol] = '-';
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armor}");
+                    break;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            foreach (var row in battleField)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+
+        static bool IsCellValid(int armyRow, int armyCol, int rows, int cols)
+        {
+            return armyRow >= 0 && armyCol >= 0 && armyRow < rows && armyCol < cols;
+        }
+    }
+}
